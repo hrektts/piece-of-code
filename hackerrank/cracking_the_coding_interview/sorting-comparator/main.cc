009725f2f9f294bc8d6ca7bac5613bc2f43eba13
Katsutoshi Horie <mps299792458@gmail.com>
2017-07-31 23:02:33 +0900

    Add a solved challenge
    * Algorithms / Cracking the Coding Interview / Sorting: Comparator

---
+++ hackerrank/cracking_the_coding_interview/sorting-comparator/main.cc
@@ -0,0 +1,45 @@
+#include <cmath>
+#include <cstdio>
+#include <vector>
+#include <iostream>
+#include <algorithm>
+
+using namespace std;
+
+struct Player {
+    string name;
+    int score;
+};
+
+vector<Player> comparator(vector<Player> players) {
+    std::sort(players.begin(), players.end(),
+              [](const Player &a, const Player &b) {
+                  if (a.score == b.score) {
+                      return a.name < b.name;
+                  } else {
+                      return a.score > b.score;
+                  }
+              });
+    return players;
+}
+
+int main() {
+
+    int n;
+    cin >> n;
+    vector<Player> players;
+    string name;
+    int score;
+    for(int i = 0; i < n; i++){
+        cin >> name >> score;
+        Player p1;
+        p1.name = name, p1.score = score;
+        players.push_back(p1);
+    }
+
+    vector<Player> answer = comparator(players);
+    for(int i = 0; i < answer.size(); i++) {
+        cout << answer[i].name << " " << answer[i].score << endl;
+    }
+    return 0;
+}
